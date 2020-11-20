2943d77f7fb15255f40e31b57492223e1dae792a
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-11-20 08:46:04 -0700

    linechart

---
+++ src/components/LineChart.vue
@@ -0,0 +1,34 @@
+<script>
+import { Line } from "vue-chartjs";
+export default {
+  mounted() {
+    let dates = this.chartData.map((d) => d.data).reverse();
+    let totals = this.chartData.map((d) => d.total).reverse();
+
+    this.renderChart(
+      {
+        labels: dates,
+        datasets: [
+          {
+            label: this.label,
+            data: totals,
+          },
+        ],
+      },
+      this.options
+    );
+  },
+  extends: Line,
+  props: {
+    label: {
+      type: String,
+    },
+    chartData: {
+      type: Array,
+    },
+    options: {
+      type: Object,
+    },
+  },
+};
+</script>
